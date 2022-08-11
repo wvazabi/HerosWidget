eb69fa457192e3ae6844c49badb7f1a66f1aca51
Enes Kaya <eng.eneskaya@gmail.com>
2022-08-11 21:39:39 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Enes Kaya on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
